767205e3f73296a55e0f7a06e72f0001c1db8ccb
eloilsonfontenele2@gmail.com <eloilsonfontenele2@gmail.com>
2022-07-02 16:03:09 -0300

    refatoramento do codigo para programação funcional, correções de bugs, adição de css, remoção e adição de elementos html, e enfim, acho que tá pronto!😀

---
+++ README.md
@@ -0,0 +1,7 @@
+## Timer App
+
+Um simpes cronômetro feito em javascript, utilizando programação  funcional!
+
+[My Github](https://github.com/elo1lson)
+
+[Repository](https://github.com/elo1lson/timer-app)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,69 @@
-function getSeconds(sec) {
-    const data = new Date(sec * 1000);
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'UTC'
-    });
-}
+'use strict'
+
 let seconds = 0
 let secs = 0
 let started
+let timer
+const start = document.querySelector('.iniciar')
 const pause = document.querySelector('.pausar')
+const clear = document.querySelector('.zerar')
 const clock = document.querySelector('.clock')
+
+function getSeconds(sec) {
+  const data = new Date(sec * 1000);
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'UTC'
+  });
+}
+
+function checkPause() {
+  if (clock.classList.contains('paused')) {
+    clock.classList.remove('paused')
+    pause.innerHTML = 'pausar'
+    startClock(secs)
+  } else {
+    clock.classList.add('paused')
+    pause.innerHTML = 'retomar'
+    clearInterval(timer)
+  }
+}
 
 function startClock(sec) {
 
-    timer = setInterval(function () {
-        sec++;
-        clock.innerHTML = getSeconds(sec);
-        secs++
-    }, 1000);
-}
-function checkPause() {
-    if (clock.classList.contains('paused')) {
-        pause.innerHTML = 'pausar'
-        clock.classList.remove('paused')
-    }
+  timer = setInterval(function() {
+    sec++;
+    clock.innerHTML = getSeconds(sec);
+    secs++
+  }, 1000);
 }
 
-document.addEventListener('click', (e) => {
-    let el = e.target
-    if (el.classList.contains('iniciar')) {
-        if (started) return
-        checkPause()
-        startClock(seconds)
-        started = true
-    }
-    if (el.classList.contains('pausar')) {
+function clickStart() {
+  if (started) return
 
-        if (pause.innerHTML == 'retomar') {
-            pause.innerHTML ='pausar'
-            console.log(secs);
-            clock.style.animation = 'none'
-            startClock(secs)
-            return
-        }
-        clearInterval(timer)
-        clock.classList.add('paused')
-        pause.innerHTML = 'retomar'
-    }
-    if (el.classList.contains('zerar')) {
-        started = false
-        clearInterval(timer)
-        clock.innerHTML = '00:00:00'
-    }
+  start.classList.add('started')
+  started = true
+  startClock(secs)
+
 }
-)
+
+function clickPause() {
+  if (!started) return
+  checkPause()
+}
+
+function clickClear() {
+  clock.innerHTML = '00:00:00'
+  clock.classList.remove('paused')
+  pause.innerHTML = 'pausar'
+  start.classList.remove('started')
+  clearInterval(timer)
+  started = false
+  seconds = 0
+  secs = 0
+
+}
+
+start.addEventListener('click', clickStart)
+pause.addEventListener('click', clickPause)
+clear.addEventListener('click', clickClear)
